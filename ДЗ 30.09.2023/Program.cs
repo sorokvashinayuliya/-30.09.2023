3b0399bd976cbb12b41d1f9d0ad6f1b2c920431d
79176 <79176@LAPTOP-MN0I58DS>
2023-10-05 23:25:32 +0400

    Добавьте файлы проекта.

---
+++ ДЗ 30.09.2023/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace ДЗ_30._09._2023
+{
+    internal class Program
+    {
+        static double MaxCh(double x, double y)
+        {
+            return Math.Max(x, y);
+        }
+        static void ChangeRef(ref int x, ref int y)
+        {
+            (x, y) = (y, x);
+        }
+        static bool factorial(ref int num)
+        {
+            int c = num;
+            num = 1;
+            for (int i=1; i < c+1; i++)
+                try
+                {
+                    checked
+                    {
+                        num *= i;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            return true;
+        }
+        static ulong factor(ulong number)
+        {
+            try
+            {
+                if (number == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return number * factor(number - 1);
+                }
+            }
+            catch (System.StackOverflowException)
+            {
+                return 0;
+            }
+        }
+        static int nod(int a1,int b1)
+        {
+            while (b1 != 0)
+            {
+                int t = b1;
+                b1 = a1 % b1;
+                a1=t;
+            }
+            return a1;
+        }
+        static int nod(int a1,int b1,int c1)
+        {
+            return nod(a1, nod(b1, c1));
+        }
+        static int fibonachi(int k)
+        {
+            if (k == 1)
+            {
+                return 1;
+            }
+            if (k == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonachi(k - 1) + fibonachi(k - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Написать метод, возвращающий наибольшее из двух чисел");
+            try
+            {
+                Console.WriteLine("Введите два числа");
+                double a3 = Convert.ToInt32(Console.ReadLine());
+                double b3 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Максимальное число:{MaxCh(a3, b3)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число");
+            }
+            Console.WriteLine("Написать метод, который меняет местами значения двух передаваемых параметров");
+            try
+            {
+                Console.WriteLine("Введите два целых числа");
+                int s1 = Convert.ToInt32(Console.ReadLine());
+                int s2 = Convert.ToInt32(Console.ReadLine());
+                ChangeRef(ref s1, ref s2);
+                Console.WriteLine($"Первое число={s1} Второе число={s2}");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Вы ввели не число или число введено некорректно");
+            }
+            Console.WriteLine("Создать метод вычисления факториала");
+            try
+            {
+                Console.WriteLine("Введите два целых числа");
+                int chislo = Convert.ToInt32(Console.ReadLine());
+                bool flag = factorial (ref chislo);
+                Console.WriteLine(Convert.ToString(flag)+" "+chislo);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число введено некорректно");
+            }
+            Console.WriteLine("Написать рекурсивный метод вычисления факториала");
+            try
+            {
+                Console.WriteLine("Введите два целых числа");
+                ulong chislo1 = Convert.ToUInt64(Console.ReadLine());
+                Console.WriteLine("Факториал="+factor(chislo1));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число введено некорректно");
+            }
+            Console.WriteLine("Найти НОД трёх чисел");
+            try
+            {
+                Console.WriteLine("Введите три целых числа");
+                int a2 = Convert.ToInt32(Console.ReadLine());
+                int b2 = Convert.ToInt32(Console.ReadLine());
+                int c2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("НОД="+nod(a2,b2,c2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число введено некорректно");
+            }
+            Console.WriteLine("Написать рекурсивный метод, вычисляющий номер n-ого числа ряда Фибоначчи");
+            try
+            {
+                Console.WriteLine("Введите целое число");
+                int chislo2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(fibonachi(chislo2));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число введено некорректно");
+            }
+            
+        }
+
+    }
+}
---
+++ Часть 2/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Часть_2
+{
+    internal class Program
+    {
+        public static void About_Array(ref double sum, ref double increase, out double average, params double[] array)
+        {
+            average = sum = 0;
+            increase = 1;
+            foreach (double number in array)
+            {
+                sum += number;
+                increase *= number;
+            }
+            average = sum / array.Length;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 3");
+            Console.WriteLine("Введите число от 0 до 9 или exit/закрыть для завершения программы");
+            string str = Console.ReadLine();
+            if (str == "exit" || str == "закрыть")
+            {
+                System.Environment.Exit(0);
+            }
+            try
+            {
+                if (int.TryParse(str, out int num))
+                {
+                    if (num < 0 || num > 9)
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Введённое число не удовлетворяет условию");
+                        Thread.Sleep(3000);
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        if (num == 0)
+                        {
+                            Console.WriteLine(" #");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("# #");
+                            Console.WriteLine(" #");
+                        }
+                        if (num == 1)
+                        {
+                            Console.WriteLine(" #");
+                            Console.WriteLine("##");
+                            Console.WriteLine(" #");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 2)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("###");
+                            Console.WriteLine("#");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 3)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 4)
+                        {
+                            Console.WriteLine("# #");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("  #");
+                        }
+                        if (num == 5)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("# ");
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 6)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("# ");
+                            Console.WriteLine("###");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 7)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("  #");
+                        }
+                        if (num == 8)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("###");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("###");
+                        }
+                        if (num == 9)
+                        {
+                            Console.WriteLine("###");
+                            Console.WriteLine("# #");
+                            Console.WriteLine("###");
+                            Console.WriteLine("  #");
+                            Console.WriteLine("###");
+                        }
+                    }
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректно введено число");
+            }
+
+            Console.WriteLine("Упражнение 1.Переставить числа в массиве");
+            int[] numbers = new int[20];
+            Random random = new Random();
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(100);
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine("Введите номера индексы чисел, которые хотите переставить в массиве");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int temp = numbers[num1 - 1];
+            numbers[num1 - 1] = numbers[num2 - 1];
+            numbers[num2 - 1] = temp;
+            Console.WriteLine("Изменённый массив");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Упр 2");
+            Console.WriteLine("Рабоота с массивом");
+            Console.WriteLine("Укажите длинну массива");
+            try
+            {
+                if (int.TryParse(Console.ReadLine(), out int len))
+                {
+                    double[] Array_2 = new double[len];
+                    if (len == 0)
+                    {
+                        Console.WriteLine("Пустой массив");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < len; i++)
+                        {
+                            Console.Write($"Введите элемент массива номер {i + 1}: ");
+                            double num = double.Parse(Console.ReadLine());
+                            Array_2[i] = num;
+                        }
+                    }
+                    double increase = 1;
+                    double average;
+                    double sum = 0;
+                    About_Array(ref sum, ref increase, out average, Array_2);
+                    Console.WriteLine("Сумма элементов: {0}", sum);
+                    Console.WriteLine("Произведение элементов: {0}", increase);
+                    Console.WriteLine("Среднее значение: {0}", average);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неккоректно введено число");
+            }
+        }
+    }
+}
+
